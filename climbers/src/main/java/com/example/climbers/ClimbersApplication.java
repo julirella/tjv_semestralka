e8b31850ca451adb265a80248d4188877b7b6ec8
grandjul <grandjul@fit.cvut.cz>
2022-12-07 16:01:23 +0100

    created project

---
+++ climbers/src/main/java/com/example/climbers/ClimbersApplication.java
@@ -0,0 +1,13 @@
+package com.example.climbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimbersApplication.class, args);
+    }
+
+}
---
+++ climbers/src/test/java/com/example/climbers/ClimbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.climbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
