6ff5adf0de8dbfa38c33591d0c689cbeeae1f5cd
Julia Grandcourtova <grandjul@fit.cvut.cz>
2022-12-19 10:27:51 +0100

    add db info

--- climbers/src/main/java/cz/cvut/fit/tjv/climbers/Climber.java
+++
@@ -1,4 +0,0 @@
-package cz.cvut.fit.tjv.climbers;
-
-public class Climber {
-}
--- climbers/src/main/java/cz/cvut/fit/tjv/climbers/domain/Climber.java
+++ climbers/src/main/java/cz/cvut/fit/tjv/climbers/domain/Climber.java
@@ -7,6 +7,7 @@ import jakarta.persistence.OneToMany;
 import java.io.Serializable;
 import java.util.*;
 
+
 public class Climber implements Serializable, DomainEntity<Long>{
     @Id
     @GeneratedValue
@@ -18,9 +19,63 @@ public class Climber implements Serializable, DomainEntity<Long>{
     private Integer strength; //or some kind of enum??
     private Integer budget;
     @OneToMany
-    private Set<Route> routes;
+    private Set<Route> routes = new HashSet<>();
+
 
     public Long getID() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAddress() {
+        return address;
+    }
+
+    public void setAddress(Integer address) {
+        this.address = address;
+    }
+
+    public Integer getStrength() {
+        return strength;
+    }
+
+    public void setStrength(Integer strength) {
+        this.strength = strength;
+    }
+
+    public Integer getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Integer budget) {
+        this.budget = budget;
+    }
+
+    public Set<Route> getRoutes() {
+        return routes;
+    }
+
+    public void addRoute(Route route) {
+        routes.add(route);
     }
 }
