76b72034957ac0a4651a263f5dd5e8f9d9e70f46
julia <grandjul@fit.cvut.cz>
2023-11-08 10:50:13 +0100

    one parameter

--- climbers2/src/main/java/cz/cvut/fit/tjv/climbers2/api/ClimberController.java
+++ climbers2/src/main/java/cz/cvut/fit/tjv/climbers2/api/ClimberController.java
@@ -3,6 +3,7 @@ package cz.cvut.fit.tjv.climbers2.api;
 import cz.cvut.fit.tjv.climbers2.business.ClimberService;
 import cz.cvut.fit.tjv.climbers2.domain.Climber;
 import cz.cvut.fit.tjv.climbers2.domain.Route;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 
@@ -30,6 +31,7 @@ public class ClimberController extends AbstractCrudController<Climber, Long>{
      * ordered by centre price
      * */
     @Operation(description = "Recommend routes that the climber hasn't climbed yet and are within their strength range and budget.")
+    @Parameter(name = "id", description = "Id of the climber")
     @GetMapping("/recommendRoutes/{id}")
     public Iterable<Route> recommendRoutes(@PathVariable Long id){
         return service.recommendRoutes(id);
